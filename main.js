f0fd01cc188f84a20ad519421db7728c96e6dfd1
Magda02 <124370036+Magda02@users.noreply.github.com>
2024-02-14 10:26:20 +0000

    Hamburger menu

---
+++ main.js
@@ -0,0 +1,15 @@
+const hamMenu = document.querySelector('.hamburger');
+const offScreenMenu = document.querySelector('.menu');
+
+document.onclick = function(e){
+    if(e.target.id !== 'hamburger' && e.target.id !== 'menu'){
+        hamMenu.classList.remove('active');
+        offScreenMenu.classList.remove('active');
+    }
+}
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
+
