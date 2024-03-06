2731e0a5cd992ee05523a191bfc133690f245286
Magda02 <124370036+Magda02@users.noreply.github.com>
2024-03-06 17:35:48 +0000

    update

--- main.js
+++ main.js
@@ -15,21 +15,3 @@ hamMenu.addEventListener('click', () => {
 
 
 
-const form = document.querySelector("form");
-        const submitButton = document.querySelector("#submit");
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbwupmMH2bLPKfgVzYKlQa1FiWZ5k3eONCW1tquAfnoF6rp7IZQSibuZUR_H-uAaOEcaow/exec';
-        form.addEventListener('submit', e => {
-            submitButton.disabled = true;
-            e.preventDefault();
-            let requestBody = new FormData(form);
-            fetch(scriptURL, { method: 'POST', body: requestBody })
-                .then(response => response.text())  
-                .then(data => {
-                    alert('Success!', data);
-                    submitButton.disabled = false;
-                })
-                .catch(error => {
-                    alert('Error!', error.message);
-                    submitButton.disabled = false;
-                });
-        });
